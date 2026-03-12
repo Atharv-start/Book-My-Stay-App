deaa332600455871aa70e1fdfb88272cfbcfbdf0
Atharv-start <atharvraj1122@gmail.com>
2026-03-12 10:24:52 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ Appp/src/Hotel.java
@@ -0,0 +1,23 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ * This class serves as the entry point for the Book My Stay App.
+ * * @author YourName
+ * @version 1.0
+ */
+public class Hotel {
+
+    /**
+     * The main method is the starting point of the Java application.
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // Displaying the welcome message, app name, and version
+        System.out.println("-------------------------------------------------");
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Application: Hotel Booking Management System ");
+        System.out.println("Version: 1.0 ");
+        System.out.println("-------------------------------------------------");
+
+        // The program terminates naturally after this line
+    }
+}
